78c9c701103003372d151aa8dd5a22fda25ade2c
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 14:36:03 +0900

    Create const.js

---
+++ src/const.js
@@ -0,0 +1,25 @@
+//color constant
+const _RED="#f96e64";
+const _BLUE="#2196f3";
+const _WHITE="#ecf0f1";
+const _DARK_WHITE="#8dc3c7";
+const _BLACK="#262626";
+
+//ingame signal constant
+const _BACK=-1;
+const _SETTING=-2;
+const _NOMOVE=0;
+const _MOVEABLE=1;
+const _FILLER=2;
+const _CHARA=3;
+
+//cell direction constant
+const _HEX_UP=0;
+const _HEX_RIGHTUP=1;
+const _HEX_RIGHTDOWN=2;
+const _HEX_DOWN=3;
+const _HEX_LEFTDOWN=4;
+const _HEX_LEFTUP=5;
+
+//single_multy constant
+const _MULTIPLAY=0;
