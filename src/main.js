df6ac27d034f56dc1fdcf47d7fc97295a4d96007
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 14:37:29 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -57,3 +57,45 @@ function changer()
 	field.makeField(rowSlider.value, colSlider.value);
 	screenControl.set(this.field.w,this.field.h);
 }
+
+function roundedHexagon(x,y,r)
+{
+	beginShape();
+	roundedHexagonRaw(x,y,r);
+	endShape(CLOSE);
+}
+function roundedHexagonRaw(x,y,r)
+{
+	var edge=createVector(0,r*cos(PI/6));
+	var cpoint=createVector(-r/2,0);
+	var apoint=createVector(-r*7/20,0);
+	var v1, v2;
+	v2=p5.Vector.add(edge,apoint);
+	vertex(x+v2.x,y+v2.y);
+	for(var i=0;i<6;i++)
+	{
+		v1=p5.Vector.sub(edge,cpoint);
+		v2=p5.Vector.sub(edge,apoint);
+		bezierVertex(x+v1.x,y+v1.y,x+v2.x,y+v2.y,x+v2.x,y+v2.y);
+		v1=p5.Vector.add(edge,cpoint);
+		v2=p5.Vector.add(edge,apoint);
+		bezierVertex(x+v2.x,y+v2.y,x+v1.x,y+v1.y,x+v2.x,y+v2.y);
+		edge.rotate(PI/3);
+		cpoint.rotate(PI/3);
+		apoint.rotate(PI/3);
+	}
+	v1=p5.Vector.sub(edge,cpoint);
+	v2=p5.Vector.sub(edge,apoint);
+	bezierVertex(x+v1.x,y+v1.y,x+v2.x,y+v2.y,x+v2.x,y+v2.y);
+}
+
+function detectCell(kind)
+{
+	switch(kind)
+	{
+		case 3:
+		case 4:return _FILLER;
+		case 1:return _MOVEABLE;
+		default:return _NOMOVE;
+	}
+}
